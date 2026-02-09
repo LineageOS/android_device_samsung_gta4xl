a56bd59d139dd543f18d0e33ff5564f243805af2
Tim Zimmermann <tim@linux4.de>
2026-02-09 12:39:38 +0100

    gta4xl: Rename libprotobuf-cpp-full-21.7 to avoid conflicts
    Change-Id: I4609891998a4722d618aeb3e8e81eca76f3704e9

--- extract-files.py
+++ extract-files.py
@@ -30,7 +30,8 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libhidltransport.so'),
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
-            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
+            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2')
+        .replace_needed('libprotobuf-cpp-full-21.7.so', 'libprotobuf-cpp-full-21.7-u.so'),
 } # fmt: skip
 
 module = ExtractUtilsModule(
